219d20cab5f6359b66f8d959c706f27d51a0c230
AlbiDota <alisaele1@gmail.com>
2025-09-25 19:38:31 +0200

    first commit, getting started

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## npm install three
+## npm install three @types/three @react-three/fiber
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Canvas } from "@react-three/fiber";
 import './App.css';
 
+// fra youtube tutorial https://www.youtube.com/watch?v=jz-zBfTW2ew
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function Scene({children}:any) {
+        return (
+            <Canvas camera={{position:[3,2,3], fov: 45}}>
+                <color attach="background" args={["gold"]}></color>
+                {children}
+            </Canvas>
+        );
+    }
+
+    return (
+        <div className="App" style={{width:"100vw", height:"100vh"}}>
+            {/* create scene and adjust camera angle */}
+            <Scene>
+                {/* add objects */}
+                <mesh position={[0, 0, 0]}>
+                    <sphereGeometry args={[1, 64, 64]}></sphereGeometry>
+                    <meshStandardMaterial color="royalBlue" roughness={0.3} metalness={0.2}></meshStandardMaterial>
+                </mesh>
+
+                {/* add directional light */}
+                <directionalLight intensity={3}></directionalLight>
+
+                {/* add ambient light */}
+                <ambientLight intensity={0.2}></ambientLight>
+
+            </Scene>
+        </div>
+    );
 }
 
 export default App;
+
+{/* <Canvas camera={{position:[3,2,3], fov: 45}}>
+    <mesh position={[0, 0, 0]}>
+        <boxGeometry></boxGeometry>
+        <meshNormalMaterial></meshNormalMaterial>
+    </mesh>
+</Canvas> */}
